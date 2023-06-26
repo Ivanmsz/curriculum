a74e07532995313a92a70fcea538c2ebade432c2
Ivanmsz <loa.im55@gmail.com>
2023-06-25 22:20:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/", function (req, res) {
+app.get("/:universalURL", function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.listen(3000, function () {
